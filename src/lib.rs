d52664240f4a93585b84e7a522c0a47537d6615c
Davide Zarantonello <davide@zarantonello.dev>
2026-04-30 14:52:42 +0200

    fix: apply clippy lints (#8)

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ impl ESC {
         pulse_max: f64,
         pulse_neutral: f64,
     ) -> Result<Self, rppal::pwm::Error> {
-        let esc = ESC {
+        let esc = Self {
             pwm_hz,
             pulse_min,
             pulse_max,
@@ -32,22 +32,20 @@ impl ESC {
                 true,
             )?,
         };
+
         Ok(esc)
     }
 
     pub fn set_throttle(&self, value: i8) -> Result<(), pwm::Error> {
-        let value = (value.clamp(-127, 127) as f64) / 127.0;
+        let value = f64::from(value.clamp(-127, 127)) / 127.0;
 
-        let pulse;
-        if value >= 0.0 {
-            pulse = self.pulse_neutral + value * (self.pulse_max - self.pulse_neutral);
+        let pulse = if value >= 0.0 {
+            self.pulse_neutral + value * (self.pulse_max - self.pulse_neutral)
         } else {
-            pulse = self.pulse_neutral - (value.abs()) * (self.pulse_neutral - self.pulse_min);
-        }
+            self.pulse_neutral - (value.abs()) * (self.pulse_neutral - self.pulse_min)
+        };
 
-        let res = self.pwm.set_duty_cycle(pulse_to_duty(pulse, self.pwm_hz))?;
-
-        Ok(res)
+        self.pwm.set_duty_cycle(pulse_to_duty(pulse, self.pwm_hz))
     }
 }
 
@@ -58,7 +56,7 @@ impl Drop for ESC {
             .set_duty_cycle(pulse_to_duty(self.pulse_neutral, self.pwm_hz));
 
         match res {
-            Ok(_) => (),
+            Ok(()) => (),
             Err(e) => println!("{e}"),
         }
     }
@@ -81,7 +79,7 @@ impl Servo {
         pulse_neutral: f64,
         pulse_offset: f64,
     ) -> Result<Self, rppal::pwm::Error> {
-        let servo = Servo {
+        let servo = Self {
             pwm_hz,
             pulse_min,
             pulse_max,
@@ -95,24 +93,21 @@ impl Servo {
                 true,
             )?,
         };
+
         Ok(servo)
     }
 
     pub fn set_steer(&self, value: i8) -> Result<(), pwm::Error> {
-        let value = (value.clamp(-127, 127) as f64) / 127.0;
+        let value = f64::from(value.clamp(-127, 127)) / 127.0;
 
-        let pulse;
-        if value >= 0.0 {
-            pulse = (self.pulse_neutral + self.pulse_offset)
-                + value * (self.pulse_max - self.pulse_neutral);
+        let pulse = if value >= 0.0 {
+            (self.pulse_neutral + self.pulse_offset) + value * (self.pulse_max - self.pulse_neutral)
         } else {
-            pulse = (self.pulse_neutral + self.pulse_offset)
-                - (value.abs()) * (self.pulse_neutral - self.pulse_min);
-        }
+            (self.pulse_neutral + self.pulse_offset)
+                - (value.abs()) * (self.pulse_neutral - self.pulse_min)
+        };
 
-        let res = self.pwm.set_duty_cycle(pulse_to_duty(pulse, self.pwm_hz))?;
-
-        Ok(res)
+        self.pwm.set_duty_cycle(pulse_to_duty(pulse, self.pwm_hz))
     }
 }
 
@@ -123,9 +118,8 @@ impl Drop for Servo {
             self.pwm_hz,
         ));
 
-        match res {
-            Ok(_) => (),
-            Err(e) => println!("{e}"),
+        if let Err(e) = res {
+            println!("{e}");
         }
     }
 }
